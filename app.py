8352b981d2cdbdd8fce36432d06dbd8ab5f4bd58
gunjanak <janak.klal@gmail.com>
2022-10-29 13:38:20 +0545

    Basic crosstab completed

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+
 from pydoc_data.topics import topics
 from flask import Flask, render_template, request, session,redirect
 import os
@@ -24,7 +25,7 @@ row_data = ''
 
 @app.route('/',methods=("POST","GET"))
 def index():
-    img_file_path = ""
+    
     if request.method =='POST':
         if request.form.get('Submit_action')=='Submit':
             #upload file flask
@@ -39,24 +40,65 @@ def index():
             df = pd.read_csv(csv_file_path)
             print(df.head())
             column_names = df.columns.values
+            column_names = column_names.tolist()
             row_data = list(df.head().values.tolist())
+            print(column_names)
+            print(type(column_names)) 
+            tables=""
+            titles=""
+
+
+        elif request.form.get('Submit_action')=='Col_Submit':
+            print('Columns selected')
+            #The index of crosstab
+            index_list = request.form.get("df_column_selection")
+            print(index_list)
+            #The column of crosstab
+            column_list = request.form.get("df_column_selection2")
+            print(column_list)
+
+            #Calling the crosstab function of pandas
+            output_df = pd.crosstab(df[index_list],df[column_list])
+            print(output_df)
+
+            #Generating the tables for html tables
+            tables = [output_df.to_html(classes='data')]
+            #Generating the titles for html table
+            titles = output_df.columns.values
+
+            #Rereading the uploaded file and getting data 
+            csv_file_path = session.get('uploaded_csv_file_path',None)
+            print(csv_file_path)
+            df = pd.read_csv(csv_file_path)
+            print(df.head())
+            column_names = df.columns.values
+            column_names = column_names.tolist()
+            row_data = list(df.head().values.tolist())
+            print(column_names)
+
+
+            
+     
           
 
         else:
             pass
+            
 
     else:
         csv_file_path = ""
         column_names=""
         row_data=""
+        tables=""
+        titles=""
     
    
 
-    return render_template('index.html',column_names=column_names, row_data=row_data, zip=zip)
+    return render_template('index.html',column_names=column_names, row_data=row_data, tables=tables, titles=titles,zip=zip)
 
 
 
 
 #Running the app
-#if __name__ == '__main__':
- #   app.run(port=5000,debug=True)
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
